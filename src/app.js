287147d99c72a79447fc60afeabd2e95afd87149
Bruno Afonso <bruno.afonso@mutantbr.com>
2020-08-07 20:14:30 -0300

    docs: add the base config of swagger documentation

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 require('express-async-errors');
+const openapi = require('openapi-comment-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerStats = require('swagger-stats');    
+
 
 const routes = require('./routes');
 const logMiddleware = require('./middlewares/logMiddleware');
 const errorMiddleware = require('./middlewares/errorMiddleware');
+const apiDocsConfig = require('./config/openapirc');
 
+const spec = openapi(apiDocsConfig);
 const app = express();
 
 /**
@@ -16,7 +22,9 @@ app.use(logMiddleware);
 /**
  * rotas
  */
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(spec));
 app.use('/api/v1', routes.userRoutes);
+app.use(swaggerStats.getMiddleware({ swaggerSpec: require('../openapi.json'),  }));
 
 /**
  * error middleware
