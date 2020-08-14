6cfed4f96797aec002430a17632f08532f2e0d8b
Yassui Kimura <yassui.kimura@gmail.com>
2020-08-13 23:10:13 -0300

    fix: conflicts resolved with the master
    Using the lib helmet to validate deepcode suggestions.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,7 +10,7 @@ const {
 } = require('../services/userService');
 
 module.exports = {
-   /**
+    /**
     * método do controller responsável por listar todos os usuários 
     * @function getAllUsers
     * @param {import('express').Request} _req objeto request do express
@@ -20,30 +20,30 @@ module.exports = {
         const { users } = await getAllUsers();
         res.json([...users]);
     },
-   
-  /**
-   * método do controller responsável por listar os usuários pelo nome
-   * @function getUserByName
-   * @param {import('express').Request} req objeto request do express
-   * @param {import('express').Response} res objeto responde do express
-   */
+
+    /**
+     * método do controller responsável por listar os usuários pelo nome
+     * @function getUserByName
+     * @param {import('express').Request} req objeto request do express
+     * @param {import('express').Response} res objeto responde do express
+     */
     async getUserByName(req, res) {
         const user = await getUserByName(req.params.name);
         res.json(user);
     },
-    
-  /**
-   * método do controller responsável por criar um novo usuário
-   * @function createUser
-   * @param {import('express').Request} req objeto request do express
-   * @param {import('express').Response} res objeto responde do express 
-   */
+
+    /**
+     * método do controller responsável por criar um novo usuário
+     * @function createUser
+     * @param {import('express').Request} req objeto request do express
+     * @param {import('express').Response} res objeto responde do express 
+     */
     async createUser(req, res) {
         const newUser = await createUser(req.body);
         res.status(httpStatus.CREATED).json(newUser);
     },
 
-   /**
+    /**
     * método do controller responsável por excluir um usuário pelo nome
     * @param {import('express').Request} req objeto request do express
     * @param {import('express').Request} res objeto response do express
@@ -52,8 +52,8 @@ module.exports = {
         await deleteUserByName(req.params.name);
         res.sendStatus(httpStatus.NO_CONTENT);
     },
-    
-   /**
+
+    /**
     * método do controller responsavél por atualizar um usuário pelo nome
     * @param {import('express').Request} req objeto request do express
     * @param {import('express').Response} res objeto do response do express
